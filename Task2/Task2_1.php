cf52f8f01c85fdf552650ba8cb457ce2f33e534e
tsn3 <tsn3ps@gmail.com>
2021-03-20 21:59:30 +0200

    2_1

--- Task2/Task2_1.php
+++ Task2/Task2_1.php
@@ -2,16 +2,27 @@
 
 function decimalToBinary($decimalNumber)
 {
+    if (!is_numeric($decimalNumber) ) {
+        throw new Exception('Only a number from 0 and more is allowed!');
+    }
     $binary = "";
+    if ($decimalNumber == 0) {
+        return 0;
+    }
     while ($decimalNumber > 0) {
         $binary = ($decimalNumber % 2) . $binary;
         $decimalNumber = (int) ($decimalNumber / 2);
     }
     return $binary;
 }
-$binary=5;
-echo decimalToBinary ($binary);
-echo "\n";
+try {
+    $binary='word';
+    echo decimalToBinary ($binary);
+    echo "\n";
+} catch (Exception $e) {
+    echo $e->getMessage().'<br/>';
+}
+
 
 function binaryToDecimal($binaryNumber)
 {
@@ -25,41 +36,36 @@ function binaryToDecimal($binaryNumber)
     }
     return $decimal;
 }
-$decimal=10000000;
+$decimal=1000000;
 echo binaryToDecimal ($decimal). PHP_EOL;
 
-echo 'Fibonacci numbers'. PHP_EOL;
+
+echo 'Fibonacci numbers: '. PHP_EOL;
 function fibonacci($position, $a = 0, $b = 1)
 {
+    if(!is_int($position) || $position < 0){
+        throw new Exception('Only positive integer number 0 and more is allowed!');
+    }
     $row = [$a, $b];
     for ($i = 2; $i <= $position; $i++) {
         $row[] = $row[$i - 1] + $row[$i - 2];
     }
     return $row;
 }
-foreach (fibonacci(0) as $value) {
+try {
+foreach (fibonacci(-6) as $value) {
     echo $value . PHP_EOL;
 }
-
-echo 'Fibonacci numbers recursion'. PHP_EOL;
-function FibonacciRecursion($number){
-    if ($number == 0){
-        return 0;
-    } else if ($number == 1){
-        return 1;
-    } else {
-        return (FibonacciRecursion($number - 1) +
-            FibonacciRecursion($number - 2));
-    }
+} catch (Exception $e) {
+    echo $e->getMessage() . '<br/>';
 }
 
-$number = 10;
-for ($counter = 0; $counter < $number; $counter++){
-    echo FibonacciRecursion($counter),' '. PHP_EOL;
-}
 
 echo 'Number N to power M'. PHP_EOL;
 function numberPower($value, $power){
+    if ((!is_numeric($value)) || (!is_numeric($power)) ) {
+        throw new Exception('Only a number positive, negative or <0> is allowed!');
+    }
     $result = 1;
     $currentVal = $value;
     $currentPow = $power;
@@ -79,34 +85,22 @@ function numberPower($value, $power){
         return 1 / $result;
     }
 }
-$value=3;
-$power=3;
+try{
+$value=-4.7;
+$power='hello';
 echo numberPower($value, $power). PHP_EOL;
-
-echo 'Number N to power M recursion'. PHP_EOL;
-function powerRecursion($value, $power)
-{
-    if ($power == 1) {
-        return $value;
-    }
-    if ($power == 0) {
-        return 1;
-    }
-    if ($power < 0) {
-        return powerRecursion(1 / $value, -$power);
-    } else{
-        return $value * powerRecursion($value, $power - 1);
-    }
+} catch (Exception $e){
+    echo $e->getMessage() . PHP_EOL;
 }
 
-$value=3;
-$power=3;
-echo powerRecursion($value, $power). PHP_EOL;
 
 function ipFilter($ip, $lowIp, $highIp) {
     $lowIp = ip2long($lowIp);
     $highIp = ip2long($highIp);
     $ip = ip2long($ip);
+    if ($lowIp === false || $highIp === false || $ip === false) {
+        throw new Exception('Invalid IP address format');
+    }
     if ($ip <= $highIp && $lowIp <= $ip) {
         return "IP in range";
     }
@@ -114,19 +108,28 @@ function ipFilter($ip, $lowIp, $highIp) {
         return "IP out of range";
     }
 }
+try {
 $lowIp='1.127.255.100';
 $highIp='128.0.0.0';
-$ip='2.1.0.0';
+$ip='%$&&.0.<?>>.0';
 echo ipFilter($ip, $lowIp, $highIp). PHP_EOL;
+} catch (Exception $e){
+    echo $e->getMessage() . PHP_EOL;
+}
 
-$array = [0, -78, -1, 99, 0, 77, -33, 1, 3, 199, -200, 23, 25, - 1999, 0];
+
+$array = [0, -78.9, 'huihuihi', 77, -33, 1,  199, -200, 23, 25, - 1999, 0];
 
 function percent($array)
 {
+    if (!is_numeric($array) ) {
+        throw new Exception('Enter an array of numbers!'.'<br/>');
+    }
     $zero = '0';
     $positive = '0';
     $negative = '0';
     $total = count($array);
+
     foreach ($array as $num){
         if ($num > '0'){
             $positive++;
@@ -140,12 +143,21 @@ function percent($array)
     echo "Positive: ".$positive/$total*'100'.PHP_EOL;
     echo "Negative: ".$negative/$total*'100'. PHP_EOL;
     echo "Zero: ".$zero/$total*'100'.PHP_EOL;
+
 }
+
+try {
 echo  percent($array);
+} catch (Exception $e){
+    echo $e->getMessage() . PHP_EOL;
+}
 
 
 function sortOrderDesc($array)
 {
+    if (!is_numeric($array) ) {
+        throw new Exception('Enter an array of numbers!'.'<br/>');
+    }
     $count = count($array);
     for($i = 0; $i < $count; $i ++) {
         for($j = 0; $j < $count; $j ++) {
@@ -159,10 +171,18 @@ function sortOrderDesc($array)
     }
     return $array;
 }
+try {
 echo 'SortOrder DESK:' . implode(',',sortOrderDesc($array)). PHP_EOL;
+} catch (Exception $e){
+    echo $e->getMessage() . PHP_EOL;
+}
+
 
 function sortOrderAsc($array)
 {
+    if (!is_numeric($array) ) {
+        throw new Exception('Enter an array of numbers!'.'<br/>');
+    }
     $count = count($array);
     for($i = 0; $i < $count; $i ++) {
         for($j = 0; $j < $count; $j ++) {
@@ -176,8 +196,12 @@ function sortOrderAsc($array)
     }
     return $array;
 }
-
+try {
 echo 'SortOrder ASK: ' . implode(',', sortOrderAsc($array)) .PHP_EOL;
+} catch (Exception $e){
+    echo $e->getMessage() . PHP_EOL;
+}
+
 
 $matrix = array(array(1,2,3),array(4,5,6),array(7,8,9));
 function transposeMatrix($matrix)
@@ -209,7 +233,9 @@ function matrixMult($array1, $array2)
     $row = count($array1);
     $column = count($array2[0]);
     $p = count($array2);
-    if (count($array1[0]) != $p) {throw new Exception('Incompatible matrixes');}
+    if (count($array1[0]) != $p) {
+        throw new Exception('Incompatible matrixes');
+    }
     $arrayNew = [];
     for ($i = 0; $i < $row; $i++) {
         for ($j = 0; $j < $column; $j++) {
@@ -222,9 +248,13 @@ function matrixMult($array1, $array2)
     return ($arrayNew);
 }
 
+try {
 echo '<pre>';
 print_r(matrixMult($array1, $array2));
 echo '</pre>';
+} catch (Exception $e){
+    echo $e->getMessage() . PHP_EOL;
+}
 
 $arrNumbersDel = [
     [-3.3, -3.3, -99.99, 105],
@@ -257,6 +287,7 @@ echo '</pre>';
 echo "After deleting rows and columns in which the sum is positive and there is at least `0`:". '<pre>';
 print_r(deleteStringСolumn($arrNumbersDel )).PHP_EOL;
 echo '</pre>';
+
 
 $arrNumbers = array(
     '1',
